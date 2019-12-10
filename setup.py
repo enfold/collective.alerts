badf8d2bdfb7e7a43c903172cf3a787e637d11a6
Franco Pellegrini <frapell@gmail.com>
2019-12-10 15:25:43 +0000

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.alerts',
-    version='2.3.0',
+    version='2.3.1.dev0',
     description="Product for setting up site alerts",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

ed4464f5900bdfd18b4d5024b186e896bc6d1751
Franco Pellegrini <frapell@gmail.com>
2022-06-03 19:08:04 -0300

    Recompile with Module federation

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 process.traceDeprecation = true;
+const package_json = require("./package.json");
 const path = require("path");
 const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config");
+const mf_config = require("@patternslib/patternslib/webpack/webpack.mf");
 
-module.exports = async (env, argv) => {
+module.exports = (env, argv) => {
     let config = {
         entry: {
-            "jsalerts": path.resolve(__dirname, "resources/jsalerts-config"),
-        },
-	externals: {
-            window: "window",
-            $: 'jquery',
-            jquery: 'jQuery',
-            "window.jquery": 'jQuery',
-        },
+            "jsalerts.min": path.resolve(__dirname, "resources/index.js"),
+        }
     };
-    config = patternslib_config(env, argv, config, ["mockup"]);
+
+    config = patternslib_config(env, argv, config, ["jsalerts.min"]);
 
     config.output.path = path.resolve(__dirname, "src/collective/alerts/browser/static");
+
+    config.plugins.push(
+        mf_config({
+            package_json: package_json,
+            remote_entry: config.entry["jsalerts.min"],
+        })
+    );
 
     return config;
 };

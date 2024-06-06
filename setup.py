e967c633962555ef3691fe42f6eeed77f9b74ad2
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2024-06-06 15:37:43 +0000

    Back to development: 2.3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.alerts',
-    version='2.3.2',
+    version='2.3.3.dev0',
     description="Product for setting up site alerts",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
